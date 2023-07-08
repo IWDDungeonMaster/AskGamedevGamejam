f336b4c38232da876805b14d3df15f18671974bb
PavelAltynnikov <robenzon11@gmail.com>
2023-07-09 01:00:56 +0400

    change focus area 3D to 2D

---
+++ Assets/Scripts/2DScene/FocusArea.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FocusArea : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Поймали рембо!");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Потеряли рембо!");
+    }
+}
