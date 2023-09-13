19209179c4af8b6c7512d7ac8d70caa52bed3750
PavelAltynnikov <robenzon11@gmail.com>
2023-09-14 01:45:42 +0400

    add LineMovement class

---
+++ Assets/Scripts/3DScene/LineMovement.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class LineMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _leftLine;
+    [SerializeField] private Transform _centerLine;
+    [SerializeField] private Transform _rightLine;
+
+}
